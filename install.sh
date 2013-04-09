a7d720c35d245656ac0c9b96e96d60f84678901a
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2013-04-09 16:57:21 -0300

    Dependencies installation

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# pre-requisites installation
+sudo apt-get update
+sudo apt-get install -y pandoc
+sudo apt-get install -y python-pip
+
+# docopt installation through pip
+sudo pip install docopt
