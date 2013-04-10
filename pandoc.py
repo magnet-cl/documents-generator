92a38a8eb4329042785c682cb13efbc5145cde7e
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2013-04-10 20:43:44 -0300

    Pandoc operations updated to consider latex template

--- pandoc.py
+++ pandoc.py
@@ -11,3 +11,13 @@ def md_to_latex(md_file='.tmp.md', latex_file='.tmp.tex'):
 def latex_to_pdf(latex_file='.tmp.tex', pdf_file='output.pdf'):
     """ Converts from LaTeX to PDF. """
     pandoc(latex_file, '-o', pdf_file)
+
+
+def md_to_pdf(md_file='.tmp.md', pdf_file='output.pdf'):
+    """ Converts from Markdown to PDF. """
+    pandoc(md_file, '-o', pdf_file)
+
+
+def md_to_pdf_with_template(md_file, pdf_file, template):
+    """ Converts from Markdown to PDF using a template. """
+    pandoc(md_file, '-o', pdf_file, '--template={}'.format(template))
